93da974e6693f49d65db5a9d7b26d21a89d4de84
Fabricio Rocha <fabricio10.90@hotmail.com>
2018-05-03 21:11:46 -0300

    Interface Grafica

--- src/controls/Login.java
+++ src/controls/Login.java
@@ -3,10 +3,25 @@ import database.ConexaoMySQL;
 import controls.Menu;
 import javafx.fxml.FXML;
 import javafx.event.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
 public class Login {
-        public void btEntrar()
-        {
+
+        @FXML
+        public void logar() throws IOException {
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/views/Menu.fxml"));
+            primaryStage.setTitle("ControlX - Menu");
+            primaryStage.setScene(new Scene(root, 300, 250));
+            primaryStage.setResizable(false);
+            primaryStage.show();
 
         }
 }
